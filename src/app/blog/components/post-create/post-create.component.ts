633d0f9c67ba5bae930ebac07625f53a431d7b75
Jay Johnson <JayJohnson38@gmail.com>
2018-12-26 12:00:45 -0500

    added file upload (#4)
    enabled file upload functionality

--- src/app/blog/components/post-create/post-create.component.ts
+++ src/app/blog/components/post-create/post-create.component.ts
@@ -11,11 +11,12 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.scss']
 })
 export class PostCreateComponent implements OnInit {
   createPostFormGroup: FormGroup;
   post: IPost;
+  imagePreview: any;
   private postId;
   private mode = 'create';
 
@@ -34,16 +35,27 @@ export class PostCreateComponent implements OnInit {
       content: [
         '',
         Validators.compose([Validators.minLength(6), Validators.required])
-      ]
+      ],
+      image: ['', Validators.compose([Validators.required])]
     });
 
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
-        this.postService
-          .getPost(this.postId)
-          .subscribe((post: any) => (this.post = post)); // unsubscription is handled by angular httpClient
+        this.postService.getPost(this.postId).subscribe((post: any) => {
+          this.post = {
+            id: post.id,
+            title: post.title,
+            content: post.content,
+            imagePath: post.imagePath
+          };
+          this.createPostFormGroup.setValue({
+            title: this.post.title,
+            content: this.post.content,
+            image: this.post.imagePath
+          });
+        }); // unsubscription is handled by angular httpClient
       } else {
         this.mode = 'create';
         this.postId = null;
@@ -56,15 +68,28 @@ export class PostCreateComponent implements OnInit {
     const post: IPost = {
       id: null,
       title: form.value.title,
-      content: form.value.content
+      content: form.value.content,
+      imagePath: form.value.image
     };
     if (this.mode === 'create') {
       this.postService.addPost(post);
     } else {
-      this.postService.updatePost(this.postId, post.title, post.content);
+      this.postService.updatePost(this.postId, post);
     }
 
     this.resetForm(form);
+  }
+
+  onFileSeclect(event) {
+    const form = this.createPostFormGroup;
+    form.patchValue({ image: event });
+    form.get('image').updateValueAndValidity();
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(event);
   }
 
   private resetForm(formGroup: FormGroup) {
--- src/app/blog/components/post-list/post-list.component.ts
+++ src/app/blog/components/post-list/post-list.component.ts
@@ -5,7 +5,7 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent implements OnInit, OnDestroy {
   posts: IPost[] = [];
