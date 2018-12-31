88e2f0247f30ac25379268fde02bbaa988a301ee
Jamal Johnson <JayJohnson83@aol.com>
2018-12-31 16:14:56 -0500

    added auth intercepter and updated auth service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,29 +1,54 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   USER_API_URL = 'http://localhost:3000/api/users/';
+  private token: string;
+  private authStatusListener = new Subject<boolean>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getToken(): string {
+    return localStorage.getItem('token');
+  }
+
+  getIsAuthenicated(): boolean {
+    const token = this.getToken();
+    return !!token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
 
   addUser(user: IAuth) {
     const authData: IAuth = { email: user.email, password: user.password };
-    return this.http
-      .post(this.USER_API_URL + 'signup', authData)
-      .subscribe((responseData: any) => {
-        console.log(responseData);
-      });
+    return this.http.post(this.USER_API_URL + 'signup', authData).subscribe((responseData: any) => {
+      console.log(responseData);
+    });
   }
 
   login(user: IAuth) {
     const authData: IAuth = { email: user.email, password: user.password };
-    return this.http
-      .post(this.USER_API_URL + 'login', authData)
-      .subscribe((responseData: any) => {
-        console.log(responseData);
-      });
+    return this.http.post(this.USER_API_URL + 'login', authData).subscribe((responseData: any) => {
+      const token = responseData.token;
+      this.token = token;
+      if (token) {
+        localStorage.setItem('token', token);
+        this.authStatusListener.next(true);
+        this.router.navigate(['/']);
+      }
+    });
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.authStatusListener.next(false);
+    this.router.navigate(['/']);
   }
 }
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  isLoggedIn = false;
+  private authSub: Subscription;
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+    this.isLoggedIn = this.auth.getIsAuthenicated();
+    this.authSub = this.auth.getAuthStatusListener().subscribe(isAuthenticated => {
+      this.isLoggedIn = isAuthenticated;
+    });
+  }
+
+  onLogout() {
+    this.auth.logout();
+  }
+
+  ngOnDestroy() {
+    this.authSub.unsubscribe();
   }
 
 }
