082d3a59fdb39fc0925ae2548af168671dbd9131
Jamal Johnson <JayJohnson83@aol.com>
2018-12-31 16:14:16 -0500

    added backend auth check

--- backend/app.js
+++ backend/app.js
@@ -1,45 +1,36 @@
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const config = require("./config/database");
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config/database');
 
-const postsRoutes = require("./routes/posts");
-const usersRoutes = require("./routes/users");
+const postsRoutes = require('./routes/posts');
+const usersRoutes = require('./routes/users');
 
 const app = express();
 
 mongoose
-  .connect(
-    config.development.database,
-    { useNewUrlParser: true }
-  )
-  .then(() => {
-    console.log("Connected to database!");
-  })
-  .catch(err => {
-    console.log("Connection failed!", err);
-  });
+	.connect(config.development.database, { useNewUrlParser: true })
+	.then(() => {
+		console.log('Connected to database!');
+	})
+	.catch((err) => {
+		console.log('Connection failed!', err);
+	});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/images", express.static(path.join("backend/images"))); // any request is allowed from /images
+app.use('/images', express.static(path.join('backend/images'))); // any request is allowed from /images
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+	next();
 });
 
-app.use("/api/posts", postsRoutes);
-app.use("/api/users", usersRoutes);
+app.use('/api/posts', postsRoutes);
+app.use('/api/users', usersRoutes);
 
 module.exports = app;
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 
 const Post = require("../models/post");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
@@ -33,6 +34,7 @@ const storage = multer.diskStorage({
 // ADD A POST
 router.post(
   "/",
+  checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     const url = req.protocol + "://" + req.get("host");
@@ -56,6 +58,7 @@ router.post(
 // UPDATE A POST
 router.put(
   "/:id",
+  checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     let imagePath = req.body.imagePath;
@@ -94,7 +97,7 @@ router.get("/:id", (req, res, next) => {
 });
 
 // DELETE A POST
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then(result => {
     console.log(result);
     res.status(200).json({ message: "Post deleted!" });
